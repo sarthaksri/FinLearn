12f671605c597c7f636dcbf8bc661a4a366a22b1
Debaditya <debadityabarman12@gmail.com>
2024-04-21 00:58:03 +0530

    fasf

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
+import { useAuth0 } from '@auth0/auth0-react'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import StockPage from './pages/StockPage'
+
 
 function App() {
 
   return (
-    <>
-      <StockPage/>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/stocks' element={<StockPage/>}/>
+      </Routes>
+    </Router>
   )
 }
 
