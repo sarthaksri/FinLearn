a1e4079203da6f093dab80ac8b531b64ff730ef4
Debaditya <debadityabarman12@gmail.com>
2024-04-21 05:40:58 +0530

    asdfsdf

--- client/src/pages/Expenses.jsx
+++ client/src/pages/Expenses.jsx
@@ -8,13 +8,23 @@ const Expenses = () => {
     <div className='Econtainer'>
         <NavBar/>
         <Dashboard/>
+        <div className='center'>
         <div className='tranc'>
             <div className='expenseP'>
-
+              <div className='names'>
+                <span>Credit</span>
+                <span>From mom</span>
+              </div>
+              <span>+500</span>
             </div>
             <div className='expenseL'>
-                
+              <div className='names'>
+                <span>Debit</span>
+                <span>To merchant</span>
+              </div>
+              <span>-100</span>
             </div>
+        </div>
         </div>
     </div>
   )
